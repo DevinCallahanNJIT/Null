bed62de0aad5d112404b247fa2494bedcf460c88
DevinCallahanNJIT <dcc@njit.edu>
2022-10-06 13:47:47 -0400

    Added basic registration functionality

--- database/databaseServer.php
+++ database/databaseServer.php
@@ -41,6 +41,53 @@ function doLogin($username,$password)
 	//return false if not valid
 }
 
+function doRegistration($username, $password)
+{
+	//Connect to database
+	echo "Authentication Began".PHP_EOL;
+	$dbservername = "localhost";
+	$dbusername = "local";
+	$dbpassword = "ChangeLater";
+	$database = "test_db";
+	
+	// Create Connection
+	$connection = new mysqli($dbservername, $dbusername, $dbpassword, $database);
+	
+	// Check Connection
+	if($connection->connect_error) {
+		die("Connection failed: " . $connection->connect_error);
+	}
+
+	//Query Username
+	$query = "SELECT * FROM account WHERE username = \"".$username."\"";
+
+	$result = $connection->query($query);
+	
+	//Check Username Availability
+	$numRows = mysqli_num_rows($result);
+	
+	//If username unavailable, failure; else, insert username and password
+	if($numRows != 0){
+		echo "Registration Failure".PHP_EOL;
+		return array("returnCode" => "401", "message"=>"Registration Failure: This username is already taken.");
+	}else{
+		//Query Insert Data
+		$query = 'INSERT INTO account VALUES ("'.$username.'", "'.$password.'")';
+
+		if ($connection->query($query)) {
+			echo "Account Successfully Registered".PHP_EOL;
+			return array("returnCode" => "202", "message"=>"Registration Success: Your account has successfully been processed.");
+		 }
+		 if ($connection->errno) {
+			echo "Database Error: Failed To Insert Data".PHP_EOL;
+			return array("returnCode" => "404", "message"=>"Database Error: Failed to insert data into database.");
+		 }
+
+	}
+
+
+}
+
 function requestProcessor($request)
 {
 	echo "Received Request".PHP_EOL;
@@ -53,6 +100,8 @@ function requestProcessor($request)
 	{
 	case "login":
 		return doLogin($request['username'],$request['password']);
+	case "register":
+		return doRegistration($request['username'],$request['password']);
 	case "validate_session":
 		return doValidate($request['sessionId']);
 	}
