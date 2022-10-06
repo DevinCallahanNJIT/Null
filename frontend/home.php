10bb2495da1f104b17f640cff4d4801bf7d6cfd7
Devon Ludwig <dl28@njit.edu>
2022-10-06 13:58:45 -0400

    added register page

---
+++ frontend/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<nav>
+	<a href='index.php'>Login</a> |
+	<a href='register.php'>Register</a> |
+    <a href='register.php'>Home</a> |
+    <a href='register.php'>Logout</a>
+</nav>
+
+<div>
+    <h1>Home</h1>
+</div>
+
+</body>
+</html>
--- frontend/register.php
+++ frontend/register.php
@@ -16,17 +16,17 @@
 	</div>
 	<div>
 	    <label>Password</label><br>
-	    <input type="text" placeholder="Enter Password" name="password"  required><br><br>
+	    <input type="password" placeholder="Enter Password" name="password"  required><br><br>
 	</div>
-	<input type="submit" value="Login" name="Register" />
+	<input type="submit" value="Register" name="submit" />
 </div>
 <?php
-if(isset($_POST['submit']))//starts php when user clicks submit button
+if(isset($_POST['submit']))	//starts php when user clicks submit button
 {
 
-	$inputedusername= $_POST['username'];//getting username and password from the form 
-	$inputedpassword= $_POST['password'];
-	require_once('/home/ubuntu/Null/lib/rabbitMQLib.inc');//calls required files to connect to server
+	$inputedusername= $_POST['username'];	//getting username from the form 
+	$inputedpassword= $_POST['password'];	//getting password from the form
+	require_once('/home/ubuntu/Null/lib/rabbitMQLib.inc');	//calls required files to connect to server
 
 	$client = new rabbitMQClient("/home/ubuntu/Null/lib/RabbitMQ.ini","Authentication");
 	if (isset($argv[1]))
@@ -39,7 +39,7 @@ if(isset($_POST['submit']))//starts php when user clicks submit button
 	}
 
 	$request = array();
-	$request['type'] = "login";
+	$request['type'] = "register";
 	$request['username'] = $inputedusername;//sending username to server
 	$request['password'] = $inputedpassword;//sending password to server
 	$request['message'] = $msg;
@@ -49,6 +49,9 @@ if(isset($_POST['submit']))//starts php when user clicks submit button
 	echo "client received response: ".PHP_EOL;
 	print_r($response);
 	echo "\n\n";
+
+
+
 
 } 
 ?>
