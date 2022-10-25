fe75fc59b6acacac88905c8d81cb2c0f18b37c79
Ubuntu <ubuntu@host03.lab.it490>
2022-10-25 19:43:22 -0400

    Updating Database Branch

--- database/webserverCommunications.php
+++ database/webserverCommunications.php
@@ -2,22 +2,14 @@
 require_once('../lib/path.inc');
 require_once('../lib/get_host_info.inc');
 require_once('../lib/rabbitMQLib.inc');
+require_once('./connectDatabase.php');
 
 function doLogin($username,$password)
 {
 	echo "Authentication Began".PHP_EOL;
-	$dbservername = "localhost";
-	$dbusername = "local";
-	$dbpassword = "ChangeLater";
-	$database = "ProjectNull";
-	
+
 	// Create Connection
-	$connection = new mysqli($dbservername, $dbusername, $dbpassword, $database);
-	
-	// Check Connection
-	if($connection->connect_error) {
-		die("Connection failed: " . $connection->connect_error);
-	}
+	$connection = connDB();
 	
 	//Query Prepared Statement
 	$query = $connection->prepare('SELECT * FROM User WHERE username = ? AND passwordHash = ? LIMIT 1');
@@ -33,7 +25,7 @@ function doLogin($username,$password)
 	
 	//If there is more than 0 results, the credentials match.
 	if($numRows != 0){
-		echo "Login Success, Creating Session \n".PHP_EOL;
+		echo "Authentication Success\nCreating Session".PHP_EOL;
 
 		$datetime = date('Y-m-d H:i:s', time() + 86400);	//current date and time (example format: '2022-20-2022 18:46:26')
 		$sessionID = hash('sha256', $username . $datetime); //session hash based on username and current time
@@ -42,8 +34,6 @@ function doLogin($username,$password)
 		$query = $connection->prepare('INSERT INTO Session VALUES(?, ?, ?)');
 		//Binds Username Into Query Statement
 		$query->bind_param("sss", $sessionID, $username, $datetime);	
-
-		echo "about to run insert".PHP_EOL;
 
 		//Execute Query
 		if ($query->execute()) {
@@ -59,7 +49,7 @@ function doLogin($username,$password)
 		
 		//If the last query failed and gave an error, return an error
 		if ($connection->errno) {
-			echo "Database Error: Failed To Insert Data".PHP_EOL;
+			echo "Database Error: Failed To Insert Data \n".PHP_EOL;
 			return array("returnCode" => "404", "message"=>"Database Error: Logged in, failed to store session in db.");
 		}
 
@@ -72,20 +62,10 @@ function doLogin($username,$password)
 
 function doRegistration($username, $password)
 {
-	//Connect to database
 	echo "Registration Began".PHP_EOL;
-	$dbservername = "localhost";
-	$dbusername = "local";
-	$dbpassword = "ChangeLater";
-	$database = "ProjectNull";
-	
+
 	// Create Connection
-	$connection = new mysqli($dbservername, $dbusername, $dbpassword, $database);
-	
-	// Check Connection
-	if($connection->connect_error) {
-		die("Connection failed: " . $connection->connect_error);
-	}
+	$connection = connDB();
 
 	//Query Prepared Statement
 	$query = $connection->prepare('SELECT * FROM User WHERE username = ?');
@@ -122,12 +102,51 @@ function doRegistration($username, $password)
 	}
 }
 
+function doSession($sessionID, $username, $expiration){
+
+	echo "Session Validation Began".PHP_EOL;
+
+	// Create Connection
+	$connection = connDB();
+
+	//reformat $expiration into unix timestamp
+	$timestamp = strtotime($expiration);
+
+	//Query Prepared Statement
+	$query = $connection->prepare('SELECT * FROM Session WHERE sessionID = ? AND username = ? AND expiration = ? LIMIT 1');
+	//Binds Username Into Query Statement
+	$query->bind_param("sss", $sessionID, $username, $expiration);	
+	
+	//Execute Query
+	$query->execute();
+	$result = $query->get_result();
+
+	//Check for matching row
+	$numRows = mysqli_num_rows($result);
+	
+	//If valid Session data is found, check expiration date and time for expiration, else, session validation failed
+	if($numRows != 0){
+		
+		//if expiration time is valid, success, else, failure
+		if (time()<=$timestamp){
+			echo "Session Validation Success\n".PHP_EOL;
+			return array("returnCode" => "202", "message"=>"Session Validation Success: The session information is valid.");
+		}else{
+			echo "Session Validation Failure\n".PHP_EOL;
+			return array("returnCode" => "401", "message"=>"Session Validation Failure: The session is expired.");
+		}
+	}else{
+		echo "Session Validation Failure\n".PHP_EOL;
+		return array("returnCode" => "401", "message"=>"Session Validation Failure: The session ID is invalid.");
+	}
+
+}
+
 function requestProcessor($request)
 {
-	echo "Received Request".PHP_EOL;
+	echo "\nReceived Request".PHP_EOL;
 	
-	//DEBUG CODE - 1 LINE: show incoming array
-	var_dump($request);
+	//var_dump($request);
 
 	if(!isset($request['type']))
 	{
@@ -140,6 +159,9 @@ function requestProcessor($request)
 
 		case "register":
 			return doRegistration($request['username'],$request['password']);
+
+		case "session":
+			return doSession($request['sessionID'],$request['username'], $request['expiration']);
 
 		
 
---
+++ frontend/test.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<nav>
+	<a href='index.php'>Login</a> |
+	<a href='register.php'>Register</a>
+</nav>
+
+<div>
+    <h1>Login</h1>
+    <form method="POST" action="">
+	<div>
+	    <label>Username:</label><br>
+	    <input type="text" placeholder="Enter Username" name="username"  required><br><br>
+	</div>
+	<div>
+	    <label>Password</label><br>
+	    <input type="password" placeholder="Enter Password" name="password"  required><br><br>
+	</div>
+	<input type="submit" value="Login" name="submit" />
+</div>
+<?php
+if(isset($_POST['submit'])&& !empty($_POST['username']) && !empty($_POST['password']))	//starts php when user clicks submit button
+{
+
+	$inputedusername= $_POST['username'];	//getting username from the form 
+	$inputedpassword= $_POST['password'];	//getting password from the form
+	require_once('/home/ubuntu/Null/lib/rabbitMQLib.inc');	//calls required files to connect to server
+
+	$client = new rabbitMQClient("/home/ubuntu/Null/lib/RabbitMQ.ini","Authentication");
+	if (isset($argv[1]))
+	{
+		$msg = $argv[1];
+	}
+	else
+	{
+		$msg = "login info";
+	}
+
+	$time = time();
+	$sessionId = SHA1($inputedusername . $time);
+
+	$request = array();
+	$request['type'] = "login";
+	$request['username'] = $inputedusername;	//sending username to server
+	$request['password'] = $inputedpassword;	//sending password to server
+
+	$request['sessionid'] = $sessionId;			//sending session to server
+	$request['message'] = $msg;					//sending message to server
+	$response = $client->send_request($request);
+
+	$code = implode(" ",$response);	//Turns $response into a string
+	if (str_contains($code, 'Success'))	//See if response if successful
+	{
+		echo "$sessionID";
+		die(header("Location: home.php"));
+	}
+	else
+	{
+		echo "client received response: ".PHP_EOL;
+		print_r($response);
+		echo "\n\n";
+	}
+
+}
+?>
+<script>
+const { createHash } = require('crypto');
+function hash(string) {
+  return createHash('sha256').update(string).digest('hex');
+}
+</script>
+
+    </form>
+
+</body>
+</html>
