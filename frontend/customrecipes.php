fea632e0736be78d98f894b22fd715a8ac114c6f
cjh47@njit.edu <ubuntu@host04.lab.idland.local>
2022-10-29 19:21:39 -0400

    Reworked the frontend code base - Jakob

---
+++ frontend/customrecipes.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<nav>
+    <a href='home.php'>Home</a> |
+    <a href='liquorcabinet.php'>Liquor Cabinet</a> |
+    <a href='customrecipes.php'>Custom Recipes</a> |
+    <a href='logout.php'>Logout</a>
+</nav>
+
+<div>
+    <h1>Custom Recipes</h1>
+    <form method="POST" action="">
+    <div>
+        <h3>Enter the liquor that you own</h3>
+	</div>
+
+</div>
+
+<?php
+if(isset($_COOKIE["Session"])){
+    echo "Session Cookie Is Set!\n";
+    require_once('/home/ubuntu/Null/lib/rabbitMQLib.inc');	//calls required files to connect to server
+    $client = new rabbitMQClient("/home/ubuntu/Null/lib/RabbitMQ.ini","Authentication"); //connect to authentication queue
+
+    $cookieArray = (array) json_decode( $_COOKIE["Session"]);
+
+    $request = array();
+    $request['type'] = "session";
+    $request['sessionID'] = $cookieArray['sessionID'];
+    $request['username'] = $cookieArray['username'];
+    $request['expiration'] = $cookieArray['expires'];
+
+    $response = $client->send_request($request);
+
+    $code = implode(" ",$response);	//Turns $response into a string
+    
+    if (str_contains($code, 'Success'))	//See if response if successful
+    {
+        echo "<h3>".$cookieArray['username'] . "'s Liquor Cabinet</h3>";
+    }
+}else{
+    echo "<h3>Welcome Guest</h3>";
+}
+
+?>
---
+++ frontend/lib/helper.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once(__DIR__ . '/../../lib/rabbitMQLib.inc');
+require_once('logging.php');
+
+function isLoggedIn(){
+    return isset($_COOKIE['Session']);
+}
+
+function getUsername(){
+    $cookieArray = (array) json_decode( $_COOKIE["Session"]);
+    return $cookieArray['username'];
+}
+
+function getURL($path){
+    if (substr($path, 0, 1) == "/") {
+        return $path;
+    }
+    //return $_SERVER["CONTEXT_PREFIX"] . "/~ji64/$path";
+}
+
+function rabbitAuthClient($request){
+    $client = new rabbitMQClient("/home/ubuntu/Null/lib/RabbitMQ.ini","Authentication");
+    $response = $client->send_request($request);
+    return $response;
+}
+
+function isSessionValid(){
+    $cookieArray = (array) json_decode( $_COOKIE["Session"]);
+
+    $request = array();
+    $request['type'] = "session";
+    $request['sessionID'] = $cookieArray['sessionID'];
+    $request['username'] = $cookieArray['username'];
+    $request['expiration'] = $cookieArray['expires'];
+
+    $response = rabbitAuthClient($request);
+    $code = implode(" ",$response);	//Turns $response into a string
+    if (str_contains($code, 'Success')) { //See if response if successful
+        return true;
+    }	
+    
+    return false;
+
+
+}
+
+function safer_echo($var) {
+    if (!isset($var)) {
+        echo "";
+        return;
+    }
+    echo htmlspecialchars($var, ENT_QUOTES, "UTF-8");
+}
+
+?>
